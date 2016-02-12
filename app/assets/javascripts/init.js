3b3e881c1ce4b55bfd1303070c54391ee05a3282
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-02-12 09:32:46 -0500

    Initial Pokemon modal component

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,16 @@
+if (window.PokemonApp === undefined) {
+  window.PokemonApp = {};
+}
+
+PokemonApp.init = function () {
+  // 3rd party setup code here
+  console.log("Pokemon App ONLINE!");
+};
+
+
+// =============================================================================
+
+
+$(document).on("ready", function () {
+  PokemonApp.init();
+});
--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -1,2 +1,46 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+PokemonApp.Pokemon = function (pokemonUri) {
+  this.id = PokemonApp.Pokemon.idFromUri(pokemonUri);
+};
+
+PokemonApp.Pokemon.prototype.render = function () {
+  console.log(`Rendering pokemon: #${this.id}`);
+
+  $.ajax({
+    url: `/api/pokemon/${this.id}`,
+    success: function (response) {
+      console.log("Pokemon info:");
+      console.log(response);
+
+      $(".js-pkmn-name").text(response.name);
+      $(".js-pkmn-id").text(response.pkdx_id);
+      $(".js-pkmn-height").text(response.height);
+      $(".js-pkmn-weight").text(response.weight);
+
+      $(".js-pokemon-modal").modal("show");
+    },
+    error: function () {
+      alert(`Couldn't get the details of pokemon ${this.id}`)
+    }
+  });
+};
+
+PokemonApp.Pokemon.idFromUri = function (pokemonUri) {
+  var uriSegments = pokemonUri.split("/");
+  var secondLast = uriSegments.length - 2;
+
+  return uriSegments[secondLast];
+};
+
+
+// =============================================================================
+
+
+$(document).on("ready", function () {
+  $(".js-show-pokemon").on("click", function (event) {
+    var $button = $(event.currentTarget);
+    var pokemonUri = $button.data("pokemon-uri");
+
+    var pokemon = new PokemonApp.Pokemon(pokemonUri);
+    pokemon.render();
+  });
+});
